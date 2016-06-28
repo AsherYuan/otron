b8b4666657bc8463866961aabcb4945e9fd07058
asheryuan <acegikn3384@sina.com>
2016-06-28 10:36:31 +0800

    fix weather graber

--- game-server/app/graber/weather/WeatherSchedule.js
+++ game-server/app/graber/weather/WeatherSchedule.js
@@ -1,14 +1,14 @@
 var schedule = require('node-schedule');
-var Weather = require('./WeatherGraber');
+var WeatherGraber = require('./WeatherGraber');
 
 var rule = new schedule.RecurrenceRule();
 var times = [];
 //每隔一分钟
 for(var i=1; i<60; i=i+1){
-    times.push(i);
+  times.push(i);
 }
 rule.minute = times;
 
 var j = schedule.scheduleJob(rule, function(){
-	saveWeather.saveWeather();
+  WeatherGraber.saveWeather();
 });
--- game-server/app/servers/connector/cron/outdoorDataCron.js
+++ game-server/app/servers/connector/cron/outdoorDataCron.js
@@ -1,4 +1,5 @@
 var UserModel = require('../../../mongodb/models/UserModel');
+var WeatherModel = require('../../../graber/weather/WeatherModel');
 
 module.exports = function(app) {
   return new Cron(app);
@@ -13,13 +14,18 @@ Cron.prototype.currentData = function() {
   UserModel.find({}, function(err, docs) {
     if(err) console.log(err);
     else {
+      WeatherModel.find({}, function(err, docs) {
+        if(err) console.log(err);
+      });
+      
+      
+      console.log('推送消息');
       for(var i=0;i<docs.length;i++) {
         var mobile = docs[i].mobile;
         var param = {
           command:'5000',
           temp:43
         };
-        console.log('推送消息');
         self.app.get('channelService').pushMessageByUids('onMsg', param, [{
           uid: mobile,
           sid: 'user-server-1'
